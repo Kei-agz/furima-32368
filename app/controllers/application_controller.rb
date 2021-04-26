b1b8641f3e4469aa3735e39bf7d6c5e1ac193229
Kei-agz <kei.0805.bigbang@icloud.com>
2021-04-26 17:40:44 +0900

    新規登録機能、ログイン機能実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :password, :password_confirmation, :nickname, :last_name, :first_name, :last_name_read, :first_name_read, :born])
+  end
 end
