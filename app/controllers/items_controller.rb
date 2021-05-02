a887e7b4111c5eabf77cf86da7d4ac3aa63443c6
Kei-agz <kei.0805.bigbang@icloud.com>
2021-05-02 21:10:43 +0900

    データの登録

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,19 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
   def index
+    @items = Item.all
+  end
 
+  def new
+    @item = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :image, :explanation, :category_id, :status_id, :delivery_fee_id, :delivery_day_id, :price, :sipping_area_id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,19 @@
 class Item < ApplicationRecord
-end
+
+  belongs_to :user
+  has_one :buyer
+
+
+
+  with_options presence: true do
+    # presence:trueのみ適用されるカラムを書き出す
+   validates :name
+   validates :explanation
+   validates :category_id
+   validates :status_id
+   validates :delivery_fee_id
+   validates :delivery_day_id
+   validates :price
+   validates :sipping_area_id
+
+  end
