9f3c6040565f4d93faf592a3519c8cf8e390c1f0
Kei-agz <kei.0805.bigbang@icloud.com>
2021-05-13 22:00:01 +0900

    商品削除機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :redirect_root, only: [:edit, :update]
   def index
     @items = Item.all.order(created_at: :desc)
@@ -18,6 +18,13 @@ class ItemsController < ApplicationController
 
   end
 
+  def destroy
+    if @item.destroy
+      redirect_to root_path
+    else
+      render :show
+    end
+  end
 
     def update
       if @item.update(item_params)
