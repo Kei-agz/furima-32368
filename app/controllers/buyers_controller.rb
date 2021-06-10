fc93ac407e354ea8a15ed8b027e8fd371332f6f8
Kei-agz <kei.0805.bigbang@icloud.com>
2021-06-10 17:33:00 +0900

    soldout表示、エラー解決

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -10,10 +10,16 @@ class BuyersController < ApplicationController
   end
 
   def create
-    @form_object = FormObject.new(buyer_params)  
+    @form_object = FormObject.new(buyer_params)
      if @form_object.valid?
+      Payjp.api_key = "sk_test_1cb3e263cccbbfdf25c6c9a3"  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+      Payjp::Charge.create(
+        amount: @item.price,  # 商品の値段
+        card: buyer_params[:token],    # カードトークン
+        currency: 'jpy'                 # 通貨の種類（日本円）
+      )
       @form_object.save
-       redirect_to action: :index
+       redirect_to root_path
      else
        render action: :index
      end
@@ -21,10 +27,13 @@ class BuyersController < ApplicationController
   private 
 
   def buyer_params
-    params.require(:form_object).permit(:sipping_area_id, :city, :house_number, :building_number, :postal_code, :phone_number, :buyer_id).merge(user_id: current_user.id)
+    params.require(:form_object).permit(:sipping_area_id, :city, :house_number, :building_number, :postal_code, :phone_number, :price).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
   
-  
+  def buyer_params2
+    params.require(:form_object).permit(:price).merge(token: params[:token])
+  end
+
   def set_item
     @item = Item.find(params[:item_id])
   end
--- app/models/form_object.rb
+++ app/models/form_object.rb
@@ -1,6 +1,6 @@
 class FormObject
   include ActiveModel::Model
-  attr_accessor :sipping_area_id,:city,:house_number,:building_number,:postal_code,:phone_number,:buyer_id,:user_id,:item_id 
+  attr_accessor :sipping_area_id,:city,:house_number,:building_number,:postal_code,:phone_number,:user_id,:item_id,:token
 
   
   with_options presence: true do
@@ -8,7 +8,6 @@ class FormObject
   validates :sipping_area_id
   validates :city
   validates :house_number
-  validates :buyer_id
     
     with_options format: { with:/\A\d{3}[-]\d{4}\z/ } do
       validates :postal_code
@@ -20,7 +19,7 @@ class FormObject
  
 
   def save
-  buyer = Buyer.create(user_id: buyer.id, item_id: item.id)
+  buyer = Buyer.create(user_id: user_id, item_id: item_id)
   Address.create(sipping_area_id: sipping_area_id, city: city, house_number: house_number, buyer_id: buyer.id, postal_code: postal_code, phone_number: phone_number)
 
   end
